3af5a4b77485957a069a906e599a99c653f3cb72
Jim Saartia <jim@saartia.fi>
2015-09-21 22:58:14 +0300

    tehty nb-projekti

---
+++ bibtexbible/src/main/java/com/miniprojekti/bibtexbible/Bibtexbible.java
@@ -0,0 +1,13 @@
+package com.miniprojekti.bibtexbible;
+
+/**
+ *
+ * @author jim@saartia.fi
+ */
+public class Bibtexbible {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ bibtexbible/src/main/java/com/miniprojekti/misc/Testiluokka.java
@@ -0,0 +1,16 @@
+package com.miniprojekti.misc;
+
+/**
+ *
+ * @author jim@saartia.fi
+ */
+public class Testiluokka {
+
+    public Testiluokka() {
+    }
+
+    public int testiMetodi() {
+        return 1;
+    }
+
+}
