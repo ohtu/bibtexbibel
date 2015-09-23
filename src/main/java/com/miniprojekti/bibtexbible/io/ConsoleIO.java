ddaec8e32c199ffd3586aa359a6a8e5d5e773580
Jim Saartia <jim@saartia.fi>
2015-09-23 13:48:10 +0300

    merge io

--- src/main/java/com/miniprojekti/bibtexbible/io/ConsoleIO.java
+++ src/main/java/com/miniprojekti/bibtexbible/io/ConsoleIO.java
@@ -2,23 +2,25 @@ package com.miniprojekti.bibtexbible.io;
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author teemu
- */
 public class ConsoleIO implements IO {
-    
-    private Scanner reader;
-    
+
+    private final Scanner scanner;
+
+    public ConsoleIO() {
+        scanner = new Scanner(System.in);
+    }
+
+    public ConsoleIO(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
     @Override
-    public String readline(int prompt) {
-        // tee jotain
-        return reader.nextLine();
+    public void write(String output) {
+        System.out.println(output);
+    }
+
+    @Override
+    public String readline() {
+        return scanner.nextLine();
     }
 }
--- src/main/java/com/miniprojekti/bibtexbible/io/IO.java
+++ src/main/java/com/miniprojekti/bibtexbible/io/IO.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.miniprojekti.bibtexbible.io;
 
-/**
- *
- * @author teemu
- */
 public interface IO {
-    
-    public String readline(int prompt);
-    
+
+    public String readline();
+
+    public void write(String output);
+
 }
